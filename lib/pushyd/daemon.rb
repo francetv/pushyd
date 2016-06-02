576b03b0879895175c68e1d44cf0ced711360baa
Bruno <bmgit@bmconseil.com>
2016-06-02 19:26:25 +0200

    daemon: create a Proxy and start it

--- lib/pushyd/daemon.rb
+++ lib/pushyd/daemon.rb
@@ -2,10 +2,19 @@ module PushyDaemon
   class Daemon
 
     def self.run(logger)
-      loop do
-        logger.info "ping"
-        sleep(1)
-      end
+      # Create a new proxy
+      p = Proxy.new(logger)
+
+      # Prepare subscriptions
+      p.prepare
+
+      # Make it listen
+
+      # Dump config table
+      puts p.table.to_s
+
+      # Start infinite loop
+      p.main
     end
 
   end
