eedccb7954d817eb90b20af316e76765ba1d960d
Bruno <bmgit@bmconseil.com>
2016-06-02 16:51:22 +0200

    improve Config object, add default configuration values

--- lib/pushyd/config.rb
+++ lib/pushyd/config.rb
@@ -3,22 +3,37 @@ require "chamber"
 class Config
   extend Chamber
 
-  def self.from options = {}
+  class << self
+    attr_reader :name
+    attr_reader :spec
+    attr_reader :files
+    attr_reader :version
+    attr_reader :env
+  end
 
-    # Check parameters
-    return unless options[:root] && options[:env]
+  def self.prepare args = {}
+    # Context parameters
+    raise "config: missing root" unless (@root = args[:root])
+    raise "config: missing env"  unless (@env = args[:env])
 
-    # Init Chamber
-    configs = ["#{options[:root]}/defaults.yml"]
-    configs << options[:def_config].to_s if options[:def_config]
-    configs << options[:cmd_config].to_s if options[:cmd_config]
+    # Gemspec parameter
+    gemspec_path = "#{args[:root]}/#{args[:gemspec]}.gemspec"
+    raise "config: missing gemspec" unless args[:gemspec]
+    raise "config: missing gemspec file at #{gemspec_path}" unless File.exist?(gemspec_path)
 
-    # Display context
-    puts "Configuration  \t #{configs.join(', ')}"
-    puts "Environment   \t #{options[:env]}"
+    # Load Gemspec
+    @spec     = Gem::Specification::load gemspec_path
+    @name     = @spec.name
+    @version  = @spec.version
+    raise "config: missing name" unless @name
+
+    # Init Chamber (defaults, etc, cmdline)
+    @files = ["#{args[:root]}/defaults.yml"]
+    @files << File.expand_path("/etc/#{@name}.yml")
+    @files << args[:config].to_s if args[:config]
 
     # Load configuration files
-    load files: configs, namespaces: { environment: options[:env] }
+    load files: @files, namespaces: { environment: @env }
   end
 
   def self.dump
