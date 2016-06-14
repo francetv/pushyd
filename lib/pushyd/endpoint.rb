61b0c7ecb8de5c980e170ac0e9bad2f30c6deee0
Bruno <bmgit@bmconseil.com>
2016-06-14 18:01:48 +0200

    logging: only use class name for progname

--- lib/pushyd/endpoint.rb
+++ lib/pushyd/endpoint.rb
@@ -41,7 +41,7 @@ module PushyDaemon
       @logger.formatter = Formatter
 
       # Set progname
-      @logger.progname = sprintf(LOG_FORMAT_PROGNAME, Process.pid, me.split('::').last)
+      @logger.progname = me.split('::').last
 
       # Set expected level
       @logger.level = case loglevel
