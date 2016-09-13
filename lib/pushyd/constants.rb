ef46feaa83470a79c93548d0856542e1a4c3c388
Bruno <bmgit@bmconseil.com>
2016-09-13 16:56:26 +0200

    constants: change logging format

--- lib/pushyd/constants.rb
+++ lib/pushyd/constants.rb
@@ -16,15 +16,13 @@ AMQP_MANUAL_ACK         = false
 SHOUTER_SENTAT_DECIMALS = 6
 
 # Constants: logger
-LOG_ROTATION            = "daily"
-
 LOG_HEADER_TIME         = "%Y-%m-%d %H:%M:%S"
 LOG_HEADER_FORMAT       = "%s \t%d\t%-8s %-15s "
-LOG_MESSAGE_TRIM        = 200
+LOG_MESSAGE_TRIM        = 250
 LOG_MESSAGE_TEXT        = "%s%s"
 LOG_MESSAGE_ARRAY       = "%s     %s"
 LOG_MESSAGE_HASH        = "%s     %-20s %s\n"
 
 # Constants: logger app-specific prefix
-LOG_PREFIX_FORMAT       = nil
-
+#LOG_PREFIX_FORMAT       = "(%-12s) (%-12s)"
+LOG_PREFIX_FORMAT       = "%-20s "
