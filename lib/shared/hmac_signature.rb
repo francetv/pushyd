6ba5814d93f49394cf2615e18bbd019318528a8f
Bruno <bmgit@bmconseil.com>
2016-10-11 16:48:23 +0200

    hmac headers: always add “date”, remove duplicates, some code rewrite

--- lib/shared/hmac_signature.rb
+++ lib/shared/hmac_signature.rb
@@ -4,94 +4,88 @@ require 'base64'
 module Shared
   module HmacSignature
 
-    def headers_sign headers, config, names = ['date']
-      # Extract and check
-      return unless config.is_a? Hash
-      hmac_method = config[:method]
-      hmac_user   = config[:user]
-      hmac_secret = config[:secret]
-      #log_debug "headers_sign config", config
-
-      # Check params
-      unless config[:method] && config[:user] && config[:secret]
-        log_error "headers_sign: missing method/user/secret"
-        return
-      end
-
-      # Check params
-      unless config[:method] == 'hmac-kong'
-        log_error "headers_sign: only [hmac-kong] method is supported"
-        return
-      end
-
-      # OK, lets go
-      hmac_sign_kong headers, config[:user], config[:secret], names
-      # log_info "headers_sign: after signing", headers
-    end
-
     def headers_md5 headers, payload
       headers['Content-MD5'] = Digest::MD5.hexdigest(payload.to_s)
     end
 
+    def headers_sign headers, config
+      # Extract and check
+      return unless config.is_a? Hash
+      hmac_method   = config[:method]
+      hmac_user     = config[:user]
+      hmac_secret   = config[:secret]
+      log_debug "headers_sign config", config
+
+      # Check params
+      unless hmac_method && hmac_user && hmac_secret
+        log_error "headers_sign: missing method/user/secret"
+        return
+      end
+
+      # Check headers, and translate names to strings
+      unless config[:headers].is_a? Array
+        log_error "headers_sign: [headers] should be an array of headers to be signed"
+        return
+      end
+      hmac_headers = config[:headers].map(&:to_s)
+
+      # Update date
+      headers['Date'] = Time.now.utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+      # Let's apply the requested method
+      case hmac_method
+      when 'hmac-kong'
+        hmac_sign_kong headers, hmac_user, hmac_secret, hmac_headers
+      else
+        log_error "headers_sign: only [hmac-kong] method is supported"
+        return
+      end
+    end
+
   private
 
-    def hmac_sign_kong headers, client_id, client_secret, names
-      # Update date
-      headers['Date'] = Time.now.strftime('%a, %d %b %Y %H:%M:%S GMT')
-      # headers['Content-MD5'] = Date.now.strftime('%a, %d %b %Y %H:%M:%S GMT')
-      # log_debug "hmac_sign_kong: headers", headers
-
-      # Filter headers we're going to hash
-      myheaders = hmac_headers_filter headers, names
+    def hmac_sign_kong headers, client_id, client_secret, selection
+      # Ensure we have :date in headers and no dup
+      selected = selection.push("date").uniq
 
       # Signe string of headers
-      signature = hmac_headers_hash myheaders, client_secret
-      log_debug "hmac_sign_kong signed [#{signature}] from headers #{myheaders.keys.inspect}"
+      signature = hmac_headers_hash headers, selected, client_secret
+      log_debug "hmac_sign_kong: signed [#{signature}] from #{selected.inspect}"
 
       # Add auth header
-      headers['Authorization'] = hmac_build_header(client_id, myheaders, signature)
+      headers['Authorization'] = hmac_build_header(client_id, selection, signature)
       #headers['test'] = "testing123"
 
       # That's OK
       return headers
     end
 
-    def hmac_build_header client_id, myheaders, signature
+    def hmac_build_header client_id, selection, signature
       sprintf 'hmac username="%s", algorithm="hmac-sha1", headers="%s", signature="%s"',
         client_id,
-        myheaders.keys.map(&:downcase).join(' '),
+        selection.map(&:downcase).join(' '),
         signature
     end
 
-    def hmac_headers_filter headers, selection
-      out = {}
+    def hmac_headers_hash headers, selection, client_secret
+      # Init
+      selected = []
 
-      # Build array of keys as strings, downcase
-      selection_names = selection.map{|h| h.to_s.downcase}
+      # For each selected header
+      selection.each do |sel|
+        this = sel.downcase
 
-      # For each header, stack it or not
-      headers.each do |name, value|
-        name_down = name.downcase
-        next unless selection_names.include? name_down
-        out[name_down] = value
+        # For each header, stack it or not
+        headers.each do |header_name, header_value|
+          next unless this == header_name.downcase
+          selected << sprintf("%s: %s", this, header_value)
+        end
       end
 
-      # We're done
-      return out
-    end
-
-    def hmac_headers_hash myheaders, client_secret
-      # Build headers string
-      data = myheaders.map do |name, value|
-        sprintf("%s: %s", name, value)
-      end.join("\n")
-
-      # Hash this
+      # Build headers string and hash it
+      data = selected.join("\n")
       digest  = OpenSSL::Digest.new('sha1')
       Base64.encode64(OpenSSL::HMAC.digest(digest, client_secret, data)).strip
-    end
-
-    def hmac_sign_data client_secret, data
     end
 
   end
